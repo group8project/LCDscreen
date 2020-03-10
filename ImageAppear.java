b44c9c26b51f31368c85949078b04dfd67df21fc
AoifePower1234 <39854101+AoifePower1234@users.noreply.github.com>
2020-03-10 11:38:09 +0000

    Add files via upload

---
+++ ImageAppear.java
@@ -0,0 +1,66 @@
+
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class ImageAppear {
+	
+	
+	public static void main (String[] args) {
+		
+		
+		while (true){
+			LCD . drawString("Dino rescue",22,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",20,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",18,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",16,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",14,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",12,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",10,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",8,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",6,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",4,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",2,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",0,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",-2,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",-4,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+			LCD . drawString("Dino rescue",-6,3);
+			Delay.msDelay(1000);
+			LCD . clear();
+
+
+		}
+		
+	//	LCD . drawString("Dino rescue",5,5);
+	//	Button.ENTER.waitForPressAndRelease();
+	//	LCD . clear();
+}} 
