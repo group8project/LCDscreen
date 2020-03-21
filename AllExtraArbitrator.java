63c0d94eb0b5cee8bd40f99904e4ed629a17a9e5
AoifePower1234 <39854101+AoifePower1234@users.noreply.github.com>
2020-03-21 18:13:57 +0000

    Add files via upload

--- AllExtraArbitrator.java
+++ AllExtraArbitrator.java
@@ -1,7 +1,7 @@
 package newLejos;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
-public class SoundAndLowBatteryBehaviorArbitrator {
+public class AllExtraArbitrator {
 	//private static MovePilot turner;
 	
 	
@@ -9,11 +9,11 @@ public class SoundAndLowBatteryBehaviorArbitrator {
 		
 		Behavior b1 = new EndSoundBehaviour();
 	    Behavior b2 = new LowBattery();
-	    Behavior b3 = new PickUpObjectBehaviour();
+	    Behavior b3 = new PickUpBehaviorColour();
 	    
 	    //maybe put pickUpObjectBehavior in another array
 		
-		 Behavior [] bArray = {b3};
+		 Behavior [] bArray = {b1, b2, b3};
 	     Arbitrator arby = new Arbitrator(bArray);
 	     arby.go();
 	}}
